e599b0f9d137f905c78b285acc1bc20ac6bd31fc
sheikhzaid329 <notlegendplayz@gmail.com>
2025-08-17 13:29:50 +0500

    practice

---
+++ practice.py
@@ -0,0 +1,14 @@
+l=["ai course","3",2.4,1.0,8,2025]
+print(l)
+for i in l:
+    print(i)
+l.append('december')
+print(l)
+l.insert(1,17)
+print(l)
+l.remove(1.0)
+print(l)
+l.pop(3)
+print(l)
+l[1]=500
+print(l)
